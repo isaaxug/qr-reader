4f6eee656160442e9a0b1f9a628ad2a497fe4f0c
yutashi <katayama@xshell.io>
2018-04-18 13:55:51 +0900

    Add ability to serve MJPEG

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from flask import Flask, Response
 from pyzbar import pyzbar
 from picamera.array import PiRGBArray
 from picamera import PiCamera
@@ -15,9 +16,32 @@ camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
 time.sleep(0.1)
 
+app = Flask(__name__)
 
-def decode(image):
-    decoded_objs = pyzbar.decode(image, scan_locations=True)
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def gen():
+    while True:
+        frame = get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+def get_frame():
+    camera.capture(rawCapture, format="bgr", use_video_port=True)
+    frame = rawCapture.array
+    decoded_objs = decode(frame)
+    image = display(frame, decoded_objs)
+    ret, jpeg = cv2.imencode('.jpg', frame)
+    rawCapture.truncate(0)
+
+    return jpeg.tobytes()
+
+def decode(frame):
+    decoded_objs = pyzbar.decode(frame, scan_locations=True)
     for obj in decoded_objs:
         print(datetime.now().strftime('%H:%M:%S.%f'))
         print('Type: ', obj.type)
@@ -25,26 +49,14 @@ def decode(image):
     
     return decoded_objs
         
-def display(image, decoded_objs):
+def display(frame, decoded_objs):
     for decoded_obj in decoded_objs:
         left, top, width, height = decoded_obj.rect
-        image = cv2.rectangle(image,
+        frame = cv2.rectangle(frame,
                               (left, top),
                               (left + width, height + top),
                               (0, 255, 0), 2)
-    return image
+    return frame
     
 if __name__ == '__main__':
-    for frame in camera.capture_continuous(rawCapture,
-                                           format="bgr",
-                                           use_video_port=True):
-        image = frame.array
-        decoded_objs = decode(image)
-        image = display(image, decoded_objs)
-        cv2.imshow("Frame", image)
-        key = cv2.waitKey(1) & 0xFF
-        rawCapture.truncate(0)
-
-        if key == ord("q"):
-            break
-        
+    app.run(host="0.0.0.0", debug=False, threaded=True)
